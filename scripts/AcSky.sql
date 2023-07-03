9acb197433e1157d5af5788e6ace1b1f12cbbe0f
zero <debuggerzero@gmail.com>
2023-07-03 21:55:39 +0800

    scripts v1.4

--- scripts/AcSky.sql
+++ scripts/AcSky.sql
@@ -79,6 +79,10 @@ insert into system_resource(name, url, identity, request_method, create_date, up
 values ('批量添加用户', '/user/insert/list/userinfo', 'perms[admin]', 'POST', current_time, current_time);
 insert into system_resource(name, url, identity, request_method, create_date, update_date)
 values ('删除用户信息', '/user/delete/userinfo/*', 'perms[admin]', 'DELETE', current_time, current_time);
+insert into system_resource(name, url, identity, request_method, create_date, update_date)
+values ('获取用户列表', '/problem/list/*/*', 'anon', 'GET', current_time, current_time);
+insert into system_resource(name, url, identity, request_method, create_date, update_date)
+values ('获取单个题目信息', '/problem/one/*', 'anon', 'GET', current_time, current_time);
 
 drop table if exists system_role;
 create table system_role
@@ -120,3 +124,70 @@ create table user_role
 
 insert into user_role(user_id, role_id)
 values (1001, 1003);
+
+drop table if exists prob_info;
+CREATE TABLE prob_info
+(
+    `id`          INT PRIMARY KEY AUTO_INCREMENT COMMENT '题目id',
+    `title`       VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '题目标题',
+    `difficulty`  TINYINT(4) COMMENT '题目难度',
+    `time_limit`  INT default 1 COMMENT '时间限制(s)',
+    space_limit   INT default 64 COMMENT '空间限制(kb)',
+    `description` TEXT CHARACTER SET utf8mb4 collate utf8mb4_0900_ai_ci COMMENT '题目描述',
+    `input_desc`  TEXT CHARACTER SET utf8mb4 collate utf8mb4_0900_ai_ci COMMENT '输入格式描述',
+    `output_desc` TEXT CHARACTER SET utf8mb4 collate utf8mb4_0900_ai_ci COMMENT '输出格式描述',
+#     `sample_input`  TEXT CHARACTER SET utf8mb4 collate utf8mb4_0900_ai_ci COMMENT '样例输入',
+#     `sample_output` TEXT CHARACTER SET utf8mb4 collate utf8mb4_0900_ai_ci COMMENT '样例输出',
+    `hint`        TEXT CHARACTER SET utf8mb4 collate utf8mb4_0900_ai_ci COMMENT '提示信息',
+    `create_ts`   DATE COMMENT '添加时间',
+    `created_by`  INT COMMENT '添加者 id',
+    `is_delete`   INT DEFAULT 0 COMMENT '是否删除'
+#     `path`          VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '本地路径(图片...)',
+#     `tags`          VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '分类标签'
+)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 1
+    CHARACTER SET = utf8mb4
+    COLLATE utf8mb4_0900_ai_ci
+    COMMENT '问题信息表';
+
+insert into prob_info(title, difficulty, description, input_desc, output_desc, create_ts, created_by)
+values ('A+B', 1, '输入两个整数，求这两个整数的和是多少。', '输入两个整数A,B，用空格隔开', '输出一个整数，表示这两个数的和',
+        current_time, 1001);
+
+drop table if exists test_data;
+CREATE TABLE test_data
+(
+    `id`         INT PRIMARY KEY AUTO_INCREMENT COMMENT '数据id',
+    `input`      TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '输入数据',
+    `output`     TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '输出数据',
+    `pid`        INT NOT NULL COMMENT '题目id',
+    `is_example` INT NOT NULL DEFAULT 0 COMMENT '是否为样例'
+)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 1
+    CHARACTER SET = utf8mb4
+    COLLATE utf8mb4_0900_ai_ci
+    COMMENT '测试数据表';
+
+insert into test_data(input, output, pid, is_example)
+values ('1 2', '3', 1, 1);
+insert into test_data(input, output, pid, is_example)
+values ('1 4', '5', 1, 1);
+
+drop table if exists history;
+create table history
+(
+    `id`       int primary key auto_increment comment '记录 Id',
+    `pid`      int         not null comment '题目 Id',
+    `uid`      int         not null comment '做题人',
+    `status`   varchar(64) not null comment '题目状态',
+    `code`     text comment '代码',
+    `type`     varchar(64) not null comment '语言类型',
+    `pro_time` date        not null comment '做题时间'
+)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 1
+    CHARACTER SET = utf8mb4
+    COLLATE utf8mb4_0900_ai_ci
+    COMMENT '做题历史表';
