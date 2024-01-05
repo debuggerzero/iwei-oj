749c871b28f6de1d3b7d1e5fcffd5db4ba00e664
zero <debuggerzero@gmail.com>
2024-01-05 23:50:41 +0800

    修复已知bug

--- iwei-oj-front-end/vue.config.js
+++ iwei-oj-front-end/vue.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
     plugins: [new MonacoWebpackPlugin()],
   },
   devServer: {
+    port: 3000,
     proxy: {
       "/api": {
         target: "http://127.0.0.1:8081",
